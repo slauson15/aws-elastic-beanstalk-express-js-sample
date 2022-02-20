25f917e8110828a68a234c27be06801ce0fc0f64
slauson15 <slauson15@gmail.com>
2022-02-20 19:34:21 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! AWS learner here'));
+app.get('/', (req, res) => res.send('Successfully tested my first AWS pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
